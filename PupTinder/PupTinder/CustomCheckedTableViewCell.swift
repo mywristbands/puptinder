d0c4c1bd4205973ac2937afad931b2d1af815ba3
Alannah Woodward <alannah.woodward98@gmail.com>
2019-11-13 21:31:12 -0800

    Alannah and Hajra: table view implementations, weird double click to change checkmark

---
+++ PupTinder/PupTinder/CustomCheckedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCheckedTableViewCell.swift
+//  PupTinder
+//
+//  Created by Alannah Woodward on 11/13/19.
+//  Copyright © 2019 ecs189e. All rights reserved.
+//
+
+import UIKit
+
+class CustomCheckedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- PupTinder/PupTinder/ProfileViewController.swift
+++ PupTinder/PupTinder/ProfileViewController.swift
@@ -8,19 +8,31 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var smallDogIcon: UIButton!
     @IBOutlet weak var medDogIcon: UIButton!
     @IBOutlet weak var largeDogIcon: UIButton!
     @IBOutlet weak var femaleButton: UIButton!
     @IBOutlet weak var maleButton: UIButton!
     @IBOutlet weak var userProfilePhoto: UIImageView!
+    @IBOutlet weak var editPhotoButton: UIButton!
+    
+    @IBOutlet weak var characteristicsTV: UITableView!
+    @IBOutlet weak var personalityTV: UITableView!
+    
     
     var dogSize : String = ""
     let swiftColor = UIColor(red: 130/256, green: 94/256, blue: 246/256, alpha: 1)
     
+    var characteristics:[String] = ["Hypoallergenic", "Sheds a lot", "Kid friendly", "Drool potential", "Barks a lot"];
+    var personalityTraits:[String] = ["Friendly", "Shy", "Calm", "Submissive", "Dominant", "Energetic", "Playful", "Grumpy", "Fun-loving", "Affectionate", "Intelligent", "Inquisitive", "Fearless"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.characteristicsTV.dataSource = self
+        self.characteristicsTV.delegate = self
+        self.personalityTV.dataSource = self
+        self.personalityTV.delegate = self
         
         femaleButton.layer.masksToBounds = true
         femaleButton.layer.cornerRadius = femaleButton.frame.width/2
@@ -30,6 +42,11 @@ class ProfileViewController: UIViewController {
         
         userProfilePhoto.layer.masksToBounds = true;
         userProfilePhoto.layer.cornerRadius = userProfilePhoto.frame.width/2;
+        
+        self.characteristicsTV.allowsMultipleSelection = true
+        self.personalityTV.allowsMultipleSelection = true
+        self.characteristicsTV.reloadData()
+        self.personalityTV.reloadData()
     }
     
     @IBAction func logoutButton(_ sender: UIButton) {
@@ -109,6 +126,54 @@ class ProfileViewController: UIViewController {
             maleButton.backgroundColor = swiftColor
         } else {
             maleButton.backgroundColor = UIColor.white
+        }
+    }
+    
+    //MARK: Table View Implementation
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(tableView == self.characteristicsTV) {
+            return characteristics.count
+        } else if(tableView == self.personalityTV) {
+            return personalityTraits.count
+        }
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if(tableView == self.characteristicsTV)
+        {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Characteristic cell") ?? UITableViewCell(style: .default, reuseIdentifier: "Characteristic cell")
+            
+            let characteristic = String(format: "%@", self.characteristics[indexPath.row])
+            cell.textLabel?.text = "\(characteristic)"
+            
+            
+            cell.imageView?.image = UIImage(named: "unchecked")
+            
+            cell.isUserInteractionEnabled = true
+            return cell
+        } else if(tableView == self.personalityTV) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Personality cell") ?? UITableViewCell(style: .default, reuseIdentifier: "Personality cell")
+            let pt = String(format: "%@", self.personalityTraits[indexPath.row])
+            cell.textLabel?.text = "\(pt)"
+            
+            cell.imageView?.image = UIImage(named: "unchecked")
+            
+            cell.isUserInteractionEnabled = true
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        if(cell?.imageView?.image ?? UIImage() == UIImage(named:"unchecked")) {
+            cell?.imageView?.image = UIImage(named:"checked") ?? UIImage()
+        } else {
+            cell?.imageView?.image = UIImage(named:"unchecked") ?? UIImage()
         }
     }
 }
