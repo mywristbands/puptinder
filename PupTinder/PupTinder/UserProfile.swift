e722928d7949948713aec3431cd2d240496cdd2b
Hajra Mobashar <hmobashar@ucdavis.edu>
2019-11-16 17:06:49 -0800

    profile view tweaks with shadow

--- PupTinder/PupTinder/UserProfile.swift
+++ PupTinder/PupTinder/UserProfile.swift
@@ -14,15 +14,31 @@ class UserProfile: UIViewController {
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var sizeImage: UIImageView!
     @IBOutlet weak var genderImage: UIImageView!
+    @IBOutlet weak var imageContianer: UIView!
+    
+    let white = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1).cgColor
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.profileImage.layer.cornerRadius = self.profileImage.frame.height/2;
-        self.sizeImage.clipsToBounds = true;
-        self.sizeImage.layer.cornerRadius = self.sizeImage.frame.height/2;
-        self.genderImage.clipsToBounds = true;
-        self.genderImage.layer.cornerRadius = self.genderImage.frame.height/2;
+        self.profileImage.layer.cornerRadius = self.profileImage.frame.height/2
+        self.profileImage.clipsToBounds = true
+        
+        self.imageContianer.layer.cornerRadius = self.imageContianer.frame.height/2
+        
+       self.imageContianer.layer.shadowPath =
+              UIBezierPath(roundedRect: self.imageContianer.bounds,
+              cornerRadius: self.imageContianer.layer.cornerRadius).cgPath
+        self.imageContianer.layer.shadowColor = UIColor.black.cgColor
+        self.imageContianer.layer.shadowOpacity = 0.3
+        self.imageContianer.layer.shadowOffset = CGSize(width: 2, height: 2)
+        self.imageContianer.layer.shadowRadius = 5
+        self.imageContianer.clipsToBounds = false
+        
+        self.sizeImage.clipsToBounds = true
+        self.sizeImage.layer.cornerRadius = self.sizeImage.frame.height/2
+        self.genderImage.clipsToBounds = true
+        self.genderImage.layer.cornerRadius = self.genderImage.frame.height/2
     }
     
     override func viewWillAppear(_ animated: Bool)
@@ -43,16 +59,5 @@ class UserProfile: UIViewController {
 
         self.backgroundImage.layer.insertSublayer(gradientLayer, at: 0)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
