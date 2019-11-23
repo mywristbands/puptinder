c7f2cc23f10425776b24f8426a5b84e22affba92
Hajra Mobashar <hmobashar@ucdavis.edu>
2019-11-23 13:58:53 -0800

    fixed constraints for home view, added shadow for container, added new doggo's

--- PupTinder/PupTinder/Home.swift
+++ PupTinder/PupTinder/Home.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class Home: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var dogProfileImage: UIButton!
+    @IBOutlet weak var dogName: UIButton!
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBAction func xButton(_ sender: UIButton) {
     }
     
-
+    @IBAction func loveButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func messagesButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func logoutButton(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.containerView.layer.cornerRadius = 5
+        self.containerView.layer.shadowPath = UIBezierPath(roundedRect: self.containerView.bounds,
+        cornerRadius: self.containerView.layer.cornerRadius).cgPath
+        self.containerView.layer.shadowColor = UIColor.black.cgColor
+        self.containerView.layer.shadowOpacity = 0.7
+        self.containerView.layer.shadowOffset = CGSize(width: 25, height: 25)
+        self.containerView.layer.shadowRadius = 25
+    }
+    
 }
