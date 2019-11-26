83e4aeb9e2fed8215c5066d69127296deef14935
Alannah Woodward <alannah.woodward98@gmail.com>
2019-11-25 22:14:26 -0800

    upload for elias, getting profile not working, profile not created

--- PupTinder/PupTinder/UserProfile.swift
+++ PupTinder/PupTinder/UserProfile.swift
@@ -14,31 +14,131 @@ class UserProfile: UIViewController {
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var sizeImage: UIImageView!
     @IBOutlet weak var genderImage: UIImageView!
-    @IBOutlet weak var imageContianer: UIView!
+    @IBOutlet weak var imageContainer: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    @IBOutlet weak var aboutNameLabel: UILabel!
+    @IBOutlet weak var bioTextView: UITextView!
+    @IBOutlet weak var characteristicsCV: UICollectionView!
+    @IBOutlet weak var personalityCV: UICollectionView!
+    
+    //var userProfile: Profile = Profile(data: ["picture" : "", "name" : "", "gender" : "", "breed" : "", "size" : "", "bio" : "", "traits" : [], "characteristics" : []])
     
     let white = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1).cgColor
+    let yellow = UIColor(red: 255.0/255.0, green: 213.0/255.0, blue: 72.0/255.0, alpha: 1)
+    let purple = UIColor(red: 130.0/255.0, green: 94.0/255.0, blue: 246.0/255.0, alpha: 1)
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        Api.profiles.getProfile() { profile, error in
+            if error == nil {
+                //self.userProfile = profile ?? Profile(data: ["picture" : "", "name" : "", "gender" : "", "breed" : "", "size" : "", "bio" : "", "traits" : [], "characteristics" : []])
+                self.profileImage.image = profile?.picture//self.userProfile.picture
+                self.nameLabel.text = profile?.name//self.userProfile.name
+                self.breedLabel.text = profile?.breed//self.userProfile.breed
+                self.aboutNameLabel.text = "About \(profile?.name ?? "Mr Woofer")" //About \(self.userProfile.name)"
+                self.bioTextView.text = profile?.bio//self.userProfile.bio
+                self.sizeImage.image = self.getSizeImage(size: profile?.size ?? "", gender: profile?.gender ?? "")//self.getSizeImage(size: self.userProfile.size, gender: self.userProfile.gender)
+                self.genderImage.image = self.getGenderImage(gender: profile?.gender ?? "")//self.getGenderImage(gender: self.userProfile.gender)
+                if profile?.gender == "female" {
+                    self.genderImage.backgroundColor = self.purple
+                    self.sizeImage.backgroundColor = self.purple
+                } else {
+                    self.genderImage.backgroundColor = self.yellow
+                    self.sizeImage.backgroundColor = self.yellow
+                }
+                /*if self.userProfile.gender == "female" {
+                    self.genderImage.backgroundColor = self.purple
+                    self.sizeImage.backgroundColor = self.purple
+                } else {
+                    self.genderImage.backgroundColor = self.yellow
+                    self.sizeImage.backgroundColor = self.yellow
+                }*/
+            } else {
+                print(error ?? "ERROR")
+            }
+        }
+        
         self.profileImage.layer.cornerRadius = self.profileImage.frame.height/2
         self.profileImage.clipsToBounds = true
         
-        self.imageContianer.layer.cornerRadius = self.imageContianer.frame.height/2
+        self.imageContainer.layer.cornerRadius = self.imageContainer.frame.height/2
         
-       self.imageContianer.layer.shadowPath =
-              UIBezierPath(roundedRect: self.imageContianer.bounds,
-              cornerRadius: self.imageContianer.layer.cornerRadius).cgPath
-        self.imageContianer.layer.shadowColor = UIColor.black.cgColor
-        self.imageContianer.layer.shadowOpacity = 0.3
-        self.imageContianer.layer.shadowOffset = CGSize(width: 2, height: 2)
-        self.imageContianer.layer.shadowRadius = 5
-        self.imageContianer.clipsToBounds = false
+       self.imageContainer.layer.shadowPath =
+              UIBezierPath(roundedRect: self.imageContainer.bounds,
+              cornerRadius: self.imageContainer.layer.cornerRadius).cgPath
+        self.imageContainer.layer.shadowColor = UIColor.black.cgColor
+        self.imageContainer.layer.shadowOffset = CGSize(width: 2, height: 2)
+        self.imageContainer.layer.shadowRadius = 5
+        self.imageContainer.clipsToBounds = false
         
         self.sizeImage.clipsToBounds = true
         self.sizeImage.layer.cornerRadius = self.sizeImage.frame.height/2
         self.genderImage.clipsToBounds = true
         self.genderImage.layer.cornerRadius = self.genderImage.frame.height/2
+    }
+    
+    func getSizeImage(size: String, gender: String) -> UIImage {
+        var image = UIImage()
+        if size != "" && gender != "" {
+            switch(gender) {
+                case "female":
+                    switch(size) {
+                        case "small":
+                            image = UIImage(named: "sYellowProfile") ?? UIImage()
+                            break
+                        case "medium":
+                            image = UIImage(named: "mYellowProfile") ?? UIImage()
+                            break
+                        case "large":
+                            image = UIImage(named: "lYellowProfile") ?? UIImage()
+                            break
+                        default:
+                            break
+                    }
+                    break
+                case "male":
+                    switch(size) {
+                        case "small":
+                            image = UIImage(named: "sPurpleProfile") ?? UIImage()
+                            break
+                        case "medium":
+                            image = UIImage(named: "mPurpleProfile") ?? UIImage()
+                            break
+                        case "large":
+                            image = UIImage(named: "lPurpleProfile") ?? UIImage()
+                            break
+                        default:
+                            break
+                    }
+                    break
+                default:
+                    break
+            }
+            return image
+        } else {
+            return UIImage(named: "lPurpleProfile") ?? UIImage()
+        }
+    }
+    
+    func getGenderImage(gender: String) -> UIImage {
+        if gender != "" {
+            var image = UIImage()
+            switch(gender) {
+                case "female":
+                    image = UIImage(named: "femaleYellowProfile") ?? UIImage()
+                    break
+                case "male":
+                    image = UIImage(named: "malePurpleProfile") ?? UIImage()
+                    break
+                default:
+                    break
+            }
+            return image
+        } else {
+            return UIImage(named: "malePurpleProfile") ?? UIImage()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool)
