2b2990451defe63082f89b5e796c8c227b3262c3
Tammy Lee <tttlee@ucdavis.edu>
2019-12-03 21:45:46 -0800

    to be safe

--- PupTinder/PupTinder/Converstions.swift
+++ PupTinder/PupTinder/Converstions.swift
@@ -8,29 +8,54 @@
 
 import UIKit
 
-class Converstions: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-    
+class Converstions: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var matchesCollection: UICollectionView!
+    @IBOutlet weak var conversationsTV: UITableView!
     
     var profilesArray: [Profile] = []
+    var conversationPartnersArray: [Profile] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.matchesCollection.delegate = self
         self.matchesCollection.dataSource = self
-
+        self.conversationsTV.delegate = self
+        self.conversationsTV.dataSource = self
+        let dispatchGroup = DispatchGroup()
+        dispatchGroup.enter()
         Api.matches.getMatches() { profiles, error in
             if let error = error {
                 print("getMatches failed: \(error)")
             } else {
                 guard let profiles = profiles else {return}
                 self.profilesArray = profiles
-                self.matchesCollection.reloadData()
+                
             }
+            dispatchGroup.leave()
         }
+        
+        dispatchGroup.enter()
+        Api.messages.getConversationPartners() { conversationPartners, error in
+            if let error = error {
+                print("getMatches failed: \(error)")
+            } else {
+                guard let conversationPartners = conversationPartners else {return}
+                self.conversationPartnersArray = conversationPartners
+                
+            }
+            dispatchGroup.leave()
+        }
+        dispatchGroup.notify(queue: DispatchQueue.main, execute: {
+            self.matchesCollection.reloadData()
+            self.conversationsTV.reloadData()
+            print(self.conversationPartnersArray.count)
+            print("Done with both calls")
+        })
+        
+        
     }
     
-    // Collection View Protocol
+    // Collection View Protocol for Matches
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return profilesArray.count
     }
@@ -45,6 +70,19 @@ class Converstions: UIViewController, UICollectionViewDelegate, UICollectionView
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 50.0, height: 50.0)
+    }
+    
+    // Table View Protocol for Conversations
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(conversationPartnersArray.count)
+        return conversationPartnersArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "conversationCell", for: indexPath as IndexPath) as! ConvoTableViewCell
+        let name = self.conversationPartnersArray[indexPath.item].name
+        cell.convoLabel?.text = name
+        return cell
     }
 
 }
---
+++ PupTinder/PupTinder/ConvoTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ConvoTableViewCell.swift
+//  PupTinder
+//
+//  Created by Tammy Lee on 12/3/19.
+//  Copyright © 2019 ecs189e. All rights reserved.
+//
+
+import UIKit
+
+class ConvoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var convoLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
